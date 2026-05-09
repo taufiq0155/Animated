61b2054ee9a3dd42bdf4b13caee26dc7fe533d1a
taufiq0155 <taufiqrahman015@gmail.com>
2026-05-10 00:23:04 +0600

    first commit

---
+++ Frontend/src/constants/index.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+
+            {label: 'Store'},
+            {label: 'Mac'},
+            {label: 'iphone'},
+            {label: 'Vision'},
+            {label: 'Airpods'},
+            
+          ]
