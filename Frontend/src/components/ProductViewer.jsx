0128e307109e86c746bdafa75bc50917406251d4
taufiq0155 <taufiqrahman015@gmail.com>
2026-05-10 16:24:54 +0600

    3D box is done

--- Frontend/src/components/ProductViewer.jsx
+++ Frontend/src/components/ProductViewer.jsx
@@ -1,5 +1,8 @@
 import clsx from "clsx";
 import useMacBookStore from "../store";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
+
 
 const ProductViewer = () => {
     const { color, setColor, setScale, scale } = useMacBookStore();
@@ -51,9 +54,15 @@ const ProductViewer = () => {
 
 
             </div>
-            <p className="text-white text-4xl">Render Canvas</p>
+            <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
+                {/* <ambientLight intensity={1} />
+                <directionalLight position={[3, 3, 5]} intensity={1.5} /> */}
+                <Box position={[0,0,0]} scale={10 * scale} material-color={color} />
+                <OrbitControls enableZoom={false} />
+            </Canvas>
         </section>
     )
 }
 
 export default ProductViewer
+
