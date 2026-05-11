e215960e7d96b8720c2a112deda1e9fe9c67c70d
taufiq0155 <taufiqrahman015@gmail.com>
2026-05-11 17:40:59 +0600

    Feature section done

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -1,14 +1,16 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
-const useMacBookStore = create((set)=>({
+const useMacbookStore = create((set) => ({
     color: '#2e2c2e',
-    setColor: (color) => set({color}),
+    setColor: (color) => set({ color }),
 
-    scale:0.08,
-    setScale:(scale)=> set({scale}),
+    scale: 0.08,
+    setScale: (scale) => set({ scale }),
 
-    reset: ()=> set({color:'#2e2c2e', scale:0.08 }),
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
 
-
+    reset: () => set({ color: '#2e2c2e', scale: 0.08, texture: '/videos/feature-1.mp4' }),
 }))
-export default useMacBookStore;
+
+export default useMacbookStore;
